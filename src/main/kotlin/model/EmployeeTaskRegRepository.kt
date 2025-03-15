307caaad761661aafd9a7bf8e04bec86c22da810
Leturgone <finekon221@gmail.com>
2025-03-15 21:56:12 +0300

    Добавлена функция добавления юзера

--- src/main/kotlin/model/EmployeeTaskRegRepository.kt
+++ src/main/kotlin/model/EmployeeTaskRegRepository.kt
@@ -2,4 +2,7 @@ package model
 
 interface EmployeeTaskRegRepository {
     suspend fun allTasks():List<Task>
+    suspend fun addTask()
+
+    suspend fun addUser(login:String, passwordHash:String)
 }
--- src/main/kotlin/model/EmployeeTaskRegRepositoryImpl.kt
+++ src/main/kotlin/model/EmployeeTaskRegRepositoryImpl.kt
@@ -1,5 +1,6 @@
 package model
 
+import db.AppUserDAO
 import db.TaskDAO
 import db.daoToTaskModel
 import db.suspendTransaction
@@ -8,4 +9,17 @@ class EmployeeTaskRegRepositoryImpl:EmployeeTaskRegRepository {
     override suspend fun allTasks(): List<Task> = suspendTransaction {
         TaskDAO.all().map(::daoToTaskModel)
     }
+
+    override suspend fun addTask() {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun addUser(login: String, passwordHash: String): Unit = suspendTransaction{
+        AppUserDAO.new {
+            this.login = login
+            this.passwordHash = passwordHash
+            this.role = "employee"
+        }
+    }
+
 }
