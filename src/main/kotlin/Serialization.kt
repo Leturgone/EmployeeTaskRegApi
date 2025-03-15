f0f067b6c7492005202f0d998b7be14842583843
Leturgone <finekon221@gmail.com>
2025-03-15 21:58:04 +0300

    Сделан запрос регистрации

--- src/main/kotlin/Serialization.kt
+++ src/main/kotlin/Serialization.kt
@@ -1,9 +1,15 @@
+import autharization.PasswordUtils
+import io.ktor.http.*
+import io.ktor.serialization.*
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import model.AppUser
 import model.EmployeeTaskRegRepository
+import model.Requests.RegistrationRequest
 
 fun Application.configureSerialization(repository: EmployeeTaskRegRepository) {
     install(ContentNegotiation) {
@@ -16,5 +22,20 @@ fun Application.configureSerialization(repository: EmployeeTaskRegRepository) {
                 call.respond(tasks)
             }
         }
+        route("/users"){
+            post("/register"){
+                val request = call.receive<RegistrationRequest>()
+                val hashedPassword = PasswordUtils.hashPassword(request.password)
+                try {
+                    repository.addUser(request.login,hashedPassword)
+                    call.respond(HttpStatusCode.NoContent)
+                } catch (ex: IllegalStateException) {
+                    call.respond(HttpStatusCode.BadRequest)
+                } catch (ex: JsonConvertException) {
+                    call.respond(HttpStatusCode.BadRequest)
+                }
+            }
+        }
+
     }
 }
