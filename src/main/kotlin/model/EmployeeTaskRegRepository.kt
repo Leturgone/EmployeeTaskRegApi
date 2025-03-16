045107d3a20316aee9f70a2d74b470c27b6f8a5c
Leturgone <finekon221@gmail.com>
2025-03-16 23:07:28 +0300

    Добавлены функции

--- src/main/kotlin/model/EmployeeTaskRegRepository.kt
+++ src/main/kotlin/model/EmployeeTaskRegRepository.kt
@@ -2,7 +2,15 @@ package model
 
 interface EmployeeTaskRegRepository {
     suspend fun allTasks():List<Task>
+
     suspend fun addTask()
 
-    suspend fun addUser(login:String, passwordHash:String)
+    suspend fun addUser(login: String,passwordHash: String, name:String, dirName:String)
+
+    suspend fun findUserByLogin(login: String):AppUser?
+
+    suspend fun findEmployeeByUserId(userId:Int):Employee
+
+    suspend fun findDirectorByUserId(userId: Int):Director
+
 }
