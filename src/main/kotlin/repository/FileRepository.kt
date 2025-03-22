0d995d711241f90034fed19bceb9cc4360805dba
Leturgone <finekon221@gmail.com>
2025-03-22 20:36:24 +0300

    Создан репозиторий для сохранения файлов

---
+++ src/main/kotlin/repository/FileRepository.kt
@@ -0,0 +1,7 @@
+package repository
+
+interface FileRepository {
+    suspend fun uploadFile(userId:Int,category:String, fileName:String, fileBytes: ByteArray): String?
+
+    suspend fun downloadFile(filePath:String):ByteArray?
+}
