a056e5d944f22de4f6369330700bcf9eb45336f3
Leturgone <finekon221@gmail.com>
2025-03-25 10:29:49 +0300

    Добавлены отдельные файлы для routes

---
+++ src/main/kotlin/routes/profileRoutes.kt
@@ -0,0 +1,8 @@
+package routes
+
+import data.repository.EmployeeTaskRegRepository
+import io.ktor.server.routing.*
+
+fun Route.profileRoutes(repository:EmployeeTaskRegRepository){
+
+}
---
+++ src/main/kotlin/routes/userRoutes.kt
@@ -0,0 +1,46 @@
+package routes
+
+import autharization.PasswordUtils
+import autharization.Tokens
+import data.dto.LoginRequest
+import data.dto.LoginResponse
+import data.dto.RegistrationRequest
+import data.repository.EmployeeTaskRegRepository
+import io.ktor.http.*
+import io.ktor.serialization.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+fun Route.userRoutes(repository: EmployeeTaskRegRepository){
+    route("/users"){
+        post("/register"){
+            val request = call.receive<RegistrationRequest>()
+            val hashedPassword = PasswordUtils.hashPassword(request.password)
+            try {
+                repository.addUser(request.login,hashedPassword,request.name,request.dirName)
+                call.respond(HttpStatusCode.NoContent)
+            } catch (ex: IllegalStateException) {
+                call.respond(HttpStatusCode.BadRequest)
+            } catch (ex: JsonConvertException) {
+                call.respond(HttpStatusCode.BadRequest)
+            }
+        }
+        post("/login"){
+            val request = call.receive<LoginRequest>()
+
+            val user = repository.getUserByLogin(request.login)
+            if (user ==null){
+                call.respond(HttpStatusCode.Unauthorized,"Неверный логин")
+                return@post
+            }
+            if (!PasswordUtils.verifyPassword(request.password,user.passwordHash)){
+                call.respond(HttpStatusCode.Unauthorized,"Неверный пароль")
+                return@post
+            }
+            val token = Tokens.generateToken(user.login,user.role)
+            call.respond(HttpStatusCode.OK, LoginResponse(token))
+
+        }
+    }
+}
