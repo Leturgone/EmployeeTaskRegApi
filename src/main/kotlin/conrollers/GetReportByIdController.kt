0d0f8ad733910a835aac2c7e8931299a5c3ffada
Leturgone <finekon221@gmail.com>
2025-03-28 11:41:32 +0300

    Создан контроллер для получения отчета по id

---
+++ src/main/kotlin/conrollers/GetReportByIdController.kt
@@ -0,0 +1,32 @@
+package conrollers
+
+import data.model.Report
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.auth.*
+import io.ktor.server.auth.jwt.*
+
+import io.ktor.server.response.*
+import servicies.ReportService
+
+class GetReportByIdController(private val reportService: ReportService) {
+    suspend fun handle(call:ApplicationCall){
+        val principal = call.principal<JWTPrincipal>()
+        val login = principal?.payload?.getClaim("login")?.asString()
+        val reportId = call.parameters["reportId"]?.toInt()
+        if (reportId == null) {
+            call.respond(HttpStatusCode.BadRequest)
+            return
+        }
+        if (login!=null){
+            reportService.getReportById(reportId).onSuccess { report ->
+                call.respond(HttpStatusCode.OK, report)
+            }.onFailure {
+                call.respond(HttpStatusCode.NotFound,"Report not found")
+            }
+        }
+        else {
+            call.respond(HttpStatusCode.BadRequest, "Invalid token")
+        }
+    }
+}
