591fac056e5289bc004146ba61dafc344fa2594f
Leturgone <finekon221@gmail.com>
2025-03-28 14:46:57 +0300

    Сделан возврат ошибки

--- src/main/kotlin/conrollers/GetReportByIdController.kt
+++ src/main/kotlin/conrollers/GetReportByIdController.kt
@@ -13,9 +13,9 @@ class GetReportByIdController(private val reportService: ReportService) {
     suspend fun handle(call:ApplicationCall){
         val principal = call.principal<JWTPrincipal>()
         val login = principal?.payload?.getClaim("login")?.asString()
-        val reportId = call.parameters["reportId"]?.toInt()
+        val reportId = call.parameters["reportId"]?.toIntOrNull()
         if (reportId == null) {
-            call.respond(HttpStatusCode.BadRequest)
+            call.respond(HttpStatusCode.BadRequest,"Invalid report id")
             return
         }
         if (login!=null){
