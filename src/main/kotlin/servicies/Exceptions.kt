c37c1daebb244581bd751a2582c9ee066aed2031
Leturgone <finekon221@gmail.com>
2025-03-28 13:46:55 +0300

    Добавлены ошибки

--- src/main/kotlin/servicies/Exceptions.kt
+++ src/main/kotlin/servicies/Exceptions.kt
@@ -4,3 +4,7 @@ package servicies
 class FilePathException(): Exception()
 
 class DownloadFileException():Exception()
+
+class AuthException():Exception()
+
+class InvalidLoginException():Exception()
