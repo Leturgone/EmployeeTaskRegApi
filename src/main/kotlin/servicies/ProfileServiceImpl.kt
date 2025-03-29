dabb8d895be6b4ac0d53fcf7978040021652dfd7
Leturgone <finekon221@gmail.com>
2025-03-29 15:24:37 +0300

    Добавлена функция получения сотрудников

--- src/main/kotlin/servicies/ProfileServiceImpl.kt
+++ src/main/kotlin/servicies/ProfileServiceImpl.kt
@@ -1,26 +1,41 @@
 package servicies
 
 import data.model.CompanyWorker
+import data.model.Employee
 import data.repository.EmployeeTaskRegRepository
+import java.lang.NumberFormatException
 
 class ProfileServiceImpl(private val empRepository: EmployeeTaskRegRepository):ProfileService {
     override suspend fun getProfile(login:String ): Result<CompanyWorker> {
-        val user = empRepository.getUserByLogin(login)
+        val user = empRepository.getUserByLogin(login)?: return Result.failure(UserNotFoundException())
 
-         return if (user != null) {
-            when(user.role){
-                "employee" -> {
-                    Result.success(empRepository.getEmployeeByUserId(user.id))
-                }
-                "director" -> {
-                    Result.success(empRepository.getDirectorByUserId(user.id))
-                }
-                else -> Result.failure(InvalidRoleException())
+         return when(user.role){
+                 "employee" -> {
+                     Result.success(empRepository.getEmployeeByUserId(user.id))
+                 }
+                 "director" -> {
+                     Result.success(empRepository.getDirectorByUserId(user.id))
+                 }
+                 else -> Result.failure(InvalidRoleException())
+             }
 
-            }
-        } else {
-            Result.failure(UserNotFoundException())
-        }
+    }
+
+    override suspend fun getMyEmployees(login: String): Result<List<Employee>> {
+        val user = empRepository.getUserByLogin(login)?:return Result.failure(UserNotFoundException())
+
+        return when(user.role){
+           "employee" -> { Result.failure(AuthException()) }
+           "director" -> {
+               try {
+                   val dirId  = empRepository.getDirectorByUserId(user.id).id
+                   val empList = empRepository.getEmployeesByDirId(dirId)
+                   Result.success(empList)
+               }catch (ex:Exception){ Result.failure(NoSuchElementException()) }
+           }
+
+           else -> Result.failure(InvalidRoleException())
+       }
     }
 
 
