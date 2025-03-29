78c4fb2156be719668e8e910d07bcb2c672bf62a
Leturgone <finekon221@gmail.com>
2025-03-29 19:38:53 +0300

    Добавлена функция получения заданий в профиле

--- src/main/kotlin/servicies/ProfileService.kt
+++ src/main/kotlin/servicies/ProfileService.kt
@@ -2,10 +2,13 @@ package servicies
 
 import data.model.CompanyWorker
 import data.model.Employee
+import data.model.Task
 
 interface ProfileService {
 
     suspend fun getProfile(login:String): Result<CompanyWorker>
 
     suspend fun getMyEmployees(login: String):Result<List<Employee>>
+
+    suspend fun getMyTasks(login: String):Result<List<Task>>
 }
