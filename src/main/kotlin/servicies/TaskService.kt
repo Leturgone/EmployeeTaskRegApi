8fe7157baec4985e3066024938b0a8498cc3ef62
Leturgone <finekon221@gmail.com>
2025-03-29 10:58:54 +0300

    В сервис добавлен метод добавления задачи

--- src/main/kotlin/servicies/TaskService.kt
+++ src/main/kotlin/servicies/TaskService.kt
@@ -1,9 +1,12 @@
 package servicies
 
 import data.model.Task
+import io.ktor.http.content.*
 
 interface TaskService {
     suspend fun getTaskById(taskId:Int):Result<Task>
 
     suspend fun downloadTask(taskId: Int): Result<ByteArray>
+
+    suspend fun addTask(multiPartData: MultiPartData, login:String):Result<Unit>
 }
