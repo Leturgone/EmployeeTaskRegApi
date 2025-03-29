839a12697ca84fe41e87558fe24fb8db4fcba1ac
Leturgone <finekon221@gmail.com>
2025-03-29 22:20:04 +0300

    Добавлен контроллер для получения числа задач

---
+++ src/main/kotlin/controllers/GetMyTaskCountController.kt
@@ -0,0 +1,30 @@
+package controllers
+
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.auth.*
+import io.ktor.server.auth.jwt.*
+import io.ktor.server.response.*
+import servicies.*
+
+class GetMyTaskCountController(private val profileService: ProfileService){
+    suspend fun handle(call:ApplicationCall){
+        val principal = call.principal<JWTPrincipal>()
+        val login = principal?.payload?.getClaim("login")?.asString()
+        if (login != null) {
+            profileService.getMyTasksCount(login).onSuccess { count ->
+                call.respond(HttpStatusCode.OK, count)
+            }.onFailure { e ->
+                when(e){
+                    is UserNotFoundException -> call.respond(HttpStatusCode.InternalServerError, "User not found")
+                    is EmployeeNotFoundException -> call.respond(HttpStatusCode.NotFound,"Employee not found")
+                    is DirectorNotFoundException -> call.respond(HttpStatusCode.NotFound,"Director not found")
+                    is InvalidRoleException -> call.respond(HttpStatusCode.Forbidden,"Invalid role")
+                }
+            }
+        }
+        else {
+            call.respond(HttpStatusCode.BadRequest, "Invalid token")
+        }
+    }
+}
