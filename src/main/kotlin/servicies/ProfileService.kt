b2bbd80823ad02ec3f48d00743a32b294dc61159
Leturgone <finekon221@gmail.com>
2025-03-29 15:28:40 +0300

    Добавлена функция получения сотрудников

--- src/main/kotlin/servicies/ProfileService.kt
+++ src/main/kotlin/servicies/ProfileService.kt
@@ -1,14 +1,11 @@
 package servicies
 
 import data.model.CompanyWorker
-import data.model.Director
 import data.model.Employee
-import data.model.Task
 
 interface ProfileService {
 
     suspend fun getProfile(login:String): Result<CompanyWorker>
-    
 
-    //suspend fun addTask(task:Task,)
+    suspend fun getMyEmployees(login: String):Result<List<Employee>>
 }
