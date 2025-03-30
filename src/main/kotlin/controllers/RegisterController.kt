32b252de356e68fa52e57f0ca08970e0b10c8045
Leturgone <finekon221@gmail.com>
2025-03-30 22:06:36 +0300

    Создан контроллер для регистрации

---
+++ src/main/kotlin/controllers/RegisterController.kt
@@ -0,0 +1,33 @@
+package controllers
+
+import data.dto.RegistrationRequest
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.plugins.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import services.AlreadyRegisterException
+import services.InvalidEmailException
+import services.InvalidPasswordException
+import services.interfaces.UserService
+
+class RegisterController(private val userService: UserService) {
+    suspend fun handle(call:ApplicationCall){
+        try {
+            val request = call.receive<RegistrationRequest>()
+            userService.register(request).onSuccess { token ->
+                call.respond(HttpStatusCode.OK,token)
+            }.onFailure { e ->
+                when(e){
+                    is InvalidPasswordException -> call.respond(HttpStatusCode.BadRequest,"Invalid password")
+                    is InvalidEmailException -> call.respond(HttpStatusCode.BadRequest,"Invalid email")
+                    is AlreadyRegisterException -> call.respond(HttpStatusCode.Conflict,"This user already have account")
+                    is IllegalStateException -> call.respond(HttpStatusCode.BadRequest)
+                }
+            }
+        }catch (e: BadRequestException){
+            call.respond(HttpStatusCode.BadRequest,"Missing parameters")
+        }
+
+    }
+}
