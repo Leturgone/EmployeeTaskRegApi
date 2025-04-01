c690c8d4bc92f5f3e07bde727c26b86ca5f989a4
Leturgone <finekon221@gmail.com>
2025-04-01 12:06:03 +0300

    Сделаны сингелтоны

--- src/main/kotlin/di/AppModule.kt
+++ src/main/kotlin/di/AppModule.kt
@@ -1,12 +1,53 @@
 package di
 
+import controllers.*
 import data.repository.EmployeeTaskRegRepositoryImpl
 import data.repository.FileRepositoryImpl
 import domain.repository.EmployeeTaskRegRepository
 import domain.repository.FileRepository
 import org.koin.dsl.module
+import routes.params.ProfileRoutesParams
+import routes.params.ReportRoutesParams
+import routes.params.TaskRoutesParams
+import routes.params.UserRoutesParams
+import services.implementations.*
+import services.interfaces.*
 
 val appModule = module {
     single<EmployeeTaskRegRepository> {EmployeeTaskRegRepositoryImpl()}
     single<FileRepository> {FileRepositoryImpl(System.getenv("DATASTORE_PATH"))}
+
+    single<EmployeeService> {EmployeeServiceImpl(get())}
+    single<ProfileService> {ProfileServiceImpl(get())}
+    single<ReportService> {ReportServiceImpl(get(),get())}
+    single<TaskService> {TaskServiceImpl(get(),get())}
+    single<UserService> {UserServiceImpl(get())}
+
+    single<AddReportController>{AddReportController(get())}
+    single<AddTaskController>{AddTaskController(get())}
+
+    single<DownloadReportController>{ DownloadReportController(get()) }
+    single<DownloadTaskController>{DownloadTaskController(get())}
+
+    single<GetEmpByIdController>{ GetEmpByIdController(get()) }
+    single<GetEmpByNameController>{GetEmpByNameController(get())}
+
+    single<GetMyEmpController>{ GetMyEmpController(get()) }
+    single<GetMyReportsController>{ GetMyReportsController(get()) }
+    single<GetMyTasksController>{ GetMyTasksController(get()) }
+    single<GetMyTaskCountController>{ GetMyTaskCountController(get()) }
+
+    single<GetProfileController> { GetProfileController(get()) }
+    single<GetReportByIdController>{ GetReportByIdController(get()) }
+    single<GetTaskByIdController>{GetTaskByIdController(get())}
+
+    single<MarkReportController>{ MarkReportController(get()) }
+
+    single<RegisterController>{RegisterController(get())}
+    single<LoginController>{ LoginController(get()) }
+
+    single<ProfileRoutesParams>{ProfileRoutesParams(get(),get(),get(),get(),get(),get(),get(),get(),get())}
+    single<ReportRoutesParams>{ReportRoutesParams(get(),get(),get())}
+    single<TaskRoutesParams>{TaskRoutesParams(get(),get())}
+    single<UserRoutesParams>{UserRoutesParams(get(),get())}
 }
