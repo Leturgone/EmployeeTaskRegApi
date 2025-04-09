3182c369f4570347562bc4881af5e6313af24f7f
Leturgone <finekon221@gmail.com>
2025-04-09 21:32:17 +0300

    Добавлен плагин ктора

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "2.1.10"
+    id("io.ktor.plugin") version "3.1.2"
     kotlin("plugin.serialization") version "1.4.21"
     application
 }
@@ -42,9 +43,13 @@ tasks.test {
 }
 
 kotlin {
-    jvmToolchain(8)
+    jvmToolchain(21)
 }
-
+ktor {
+    fatJar {
+        //archiveFileName.set("employee.jar")
+    }
+}
 application {
     mainClass.set("ApplicationKt")
 
