e3177924790abbe4894126c8ae47610001d11100
Leturgone <finekon221@gmail.com>
2025-04-26 20:59:04 +0300

    Добавлена функция для получения количества задач для конкретного сотрудника

--- src/main/kotlin/services/interfaces/EmployeeService.kt
+++ src/main/kotlin/services/interfaces/EmployeeService.kt
@@ -7,4 +7,6 @@ interface EmployeeService {
     suspend fun getEmployeeByName(login:String,empName:String):Result<List<Employee>>
 
     suspend fun getEmployeeById(employeeId:Int):Result<Employee>
+
+    suspend fun getEmployeeTaskCountById(employeeId:Int):Result<Int>
 }
