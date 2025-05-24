c3e97e5a039b46402327311579296222aa2f42ae
Leturgone <finekon221@gmail.com>
2025-05-24 23:41:55 +0300

    added route for getting report bi task id

--- src/main/kotlin/routes/reportRoutes.kt
+++ src/main/kotlin/routes/reportRoutes.kt
@@ -8,8 +8,13 @@ fun Route.reportRoutes(reportRoutesParams: ReportRoutesParams){
     //Получение конкретного отчета
     get("/getReport/{reportId}"){ reportRoutesParams.reportByIdController.handle(call)}
 
+    //Получение отчета по id задания
+    get("/getReportByTaskId/{taskId}"){ reportRoutesParams.reportByTaskIdController.handle(call)}
+
+
     //Скачивание отчета
     get("/getReport/{reportId}/download"){reportRoutesParams.downloadReportController.handle(call)}
+
 
     //Отметка отчета статусом
     patch("/markReport/{reportId}/{status}"){reportRoutesParams.markReportController.handle(call)}
