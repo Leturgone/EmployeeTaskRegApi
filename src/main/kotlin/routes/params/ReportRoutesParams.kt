562437a9c3ce992ab45dfdf2d79f216ffcbd269c
Leturgone <finekon221@gmail.com>
2025-05-24 21:16:59 +0300

    added controller for update report

--- src/main/kotlin/routes/params/ReportRoutesParams.kt
+++ src/main/kotlin/routes/params/ReportRoutesParams.kt
@@ -3,9 +3,11 @@ package routes.params
 import controllers.DownloadReportController
 import controllers.GetReportByIdController
 import controllers.MarkReportController
+import controllers.UpdateReportController
 
 data class ReportRoutesParams(
     val reportByIdController: GetReportByIdController,
     val downloadReportController: DownloadReportController,
-    val markReportController: MarkReportController
+    val markReportController: MarkReportController,
+    val updateReportController: UpdateReportController
 )
