6f6cf4c82de6400338956be40490de3ab87c750f
Leturgone <finekon221@gmail.com>
2025-06-29 15:57:38 +0300

    added modules in Application

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -1,11 +1,10 @@
-import di.appModule
+import di.controllerModule
+import di.repositoryModule
+import di.serviceModule
 import io.ktor.server.application.*
-import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import org.koin.ktor.plugin.Koin
 import org.koin.logger.slf4jLogger
-
-
 
 
 fun main(args: Array<String>):Unit = EngineMain.main(args)
@@ -14,7 +13,7 @@ fun Application.module() {
     val config = environment.config
     install(Koin) {
         slf4jLogger()
-        modules(modules = appModule)
+        modules(repositoryModule, serviceModule, controllerModule)
     }
 
     configureAuthentication()
